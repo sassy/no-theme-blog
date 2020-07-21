20e2d4a15aaa7e7e6dc81b8932d47e7008c24524
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-07-21 21:22:10 +0900

    fix declared plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
       }
     },
     `gatsby-plugin-netlify`,
-    ` gatsby-plugin-netlify-cms`
+    `gatsby-plugin-netlify-cms`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
