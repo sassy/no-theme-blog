9c9cd3c4c194848dee112b8c48a5dd4ba692b2b8
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-07-21 21:20:12 +0900

    add plugin gatsby-plugin-netlify-cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,9 @@ module.exports = {
         name: `pages`,
         path: `${__dirname}/src/pages`
       }
-    }
+    },
+    `gatsby-plugin-netlify`,
+    ` gatsby-plugin-netlify-cms`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
